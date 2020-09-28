9b22f35f3212fbd9f41691959e9c87abb1b740e1
Jeremy Zach <44850631+Zachmanjz@users.noreply.github.com>
2020-09-28 14:06:14 -0400

    updated sayings, prompted and shake effect

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
+$(document).ready(function (){
+     var magic8ball = {};
+      $("#answer").hide();
+     magic8ball.listOfAnswers = ["It is certain", "Reply hazy try again", "cannot predict now", "yes", "Outlook not so good"];
 
-$(document).ready(function(){
- 
-var magic8Ball = {};
-magic8Ball.listOfAnswers = ["No", "Yes", "I don't think so...", "Of course!", "Indubitably", "In your dreams."];
- 
-// define the method
-magic8Ball.askQuestion = function(question) {
- 
-     // create a random number
-     var randomNumber = Math.random();
- 
-     // make the random number between 0 and the number of items in your array
-     var randomNumberForListOfAnswers = randomNumber * this.listOfAnswers.length;
- 
-     // round down the number to be a whole number
-     var randomIndex = Math.floor(randomNumberForListOfAnswers);
- 
-     // use that number to index a random number from the answers array
-     var answer = this.listOfAnswers[randomIndex];
- 
-     console.log(question);
-     console.log(answer);
-};
- 
- 
+     magic8ball.askQuestion = function(question){
+                    $("#8ball").attr("src","https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
+          $("#answer").fadeIn(4000);
+               var randomNumber = Math.random() * this.listOfAnswers.length;
+
+               var randomAnswerIndex = Math.floor(randomNumber);
+
+          var randomAnswer = this.listOfAnswers[randomAnswerIndex];
+          $("#answer").text(randomAnswer);
+
+          console.log("Question: " + question + "… " + "Answer: " + randomAnswer);
+     };
+
+     var onClick = function(){
+           $("#answer").hide();
+          $("#8ball").attr("src","https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/8side.png");
+
+          setTimeout(function(){
+               var question = prompt("ASK A YES/NO QUESTION!");
+          $("#8ball").effect("shake");
+          magic8ball.askQuestion(question);
+          },500);
+
+     };
+
+     $("#questionButton").click(onClick);
 });
-
-
-
